e40b52aafe4447ecaec50fb9e59f4075d81c234f
er_aditya_anand <er.aditya.anand@gmail.com>
2017-01-22 22:35:07 +0530

    updated DDL

--- setup.sql
+++ setup.sql
@@ -5,3 +5,31 @@ CREATE TABLE `Person` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 commit;
+
+
+CREATE TABLE `user` (
+  `UserId` int(11) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(200) NOT NULL,
+  `Email` varchar(200) NOT NULL,
+  `Mobile` varchar(15) NOT NULL,
+  `CreationDate` datetime NOT NULL,
+  `LastLoginDate` datetime DEFAULT NULL,
+  `UserStatus` varchar(45) NOT NULL,
+  `Password` varchar(45) NOT NULL,
+  PRIMARY KEY (`UserId`),
+  UNIQUE KEY `EMAIL_UNIQUE` (`Email`),
+  UNIQUE KEY `MOBILE_UNIQUE` (`Mobile`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `chantingsessionhistory` (
+  `ChantingSessionHistoryId` int(11) NOT NULL,
+  `ChantingSessionStartTime` datetime NOT NULL,
+  `ChantingSessionEndTime` datetime DEFAULT NULL,
+  `ChantingSessionDate` date NOT NULL,
+  `NumberOfBeads` int(11) DEFAULT NULL,
+  `UserId` int(11) NOT NULL,
+  PRIMARY KEY (`ChantingSessionHistoryId`),
+  KEY `UserId_idx` (`UserId`),
+  CONSTRAINT `CHANTINGSESSIONHISTORY_USER_FK` FOREIGN KEY (`UserId`) REFERENCES `user` (`UserId`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
